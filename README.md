feece233c93296d718fc5d5b1714a6cb508c8fee
404invalid-user <invaliduser@bruvland.com>
2024-08-19 18:48:55 +0100

    ip

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # parserIP
 input your ipv4, ipv6 or domain and see if it is valid
+
+## install
+
+`npm i https://github.com/MCStatusBot/parserIP.git`
+
+## example
+
+```js
+
+const ParserIP = require('parserip');
+
+
+const myIP = ParserIP('192.168.0.22');
+console.log(myIP);
+/*
+{
+  domain: null,
+  ipv4: '192.168.0.22',
+  ipv6: null,
+  port: null,
+  invalidDomain: false
+}
+*/
+
+const myIPWPort = ParserIP('192.168.0.22:8080');
+console.log(myIPWPort);
+/*
+{
+  domain: null,
+  ipv4: '192.168.0.22',
+  ipv6: null,
+  port: '8080',
+  invalidDomain: false
+}
+*/
+
+
+const myDomain = ParserIP('test.mcstatus.net');
+console.log(myDomain);
+/*
+{
+  domain: 'test.mcstatus.net',
+  ipv4: null,
+  ipv6: null,
+  port: null,
+  invalidDomain: false
+}
+*/
+
+const myIPv6 = ParserIP('[2602:f964:1:d::d]:80');
+console.log(myIPv6);
+/*
+{
+  domain: null,
+  ipv4: null,
+  ipv6: '2602:f964:1:d::d',
+  port: 80,
+  invalidDomain: false
+}
+*/
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+
+const domainPattern = /^(?<domain>[^:]+)(?::(?<port>\d+))?$/;
+const ipv4Pattern = /^(?<ipv4>\d+\.\d+\.\d+\.\d+)(?::(?<port>\d+))?$/;
+const ipv6Pattern = /^\[(?<ipv6>[0-9a-fA-F:]+)\](?::(?<port>\d+))?$/;
+const ipv6NoBracketsPattern = /^(?<ipv6>[0-9a-fA-F:]+)(?::(?<port>\d+))?$/;
+
+
+let TDLList = [];
+
+async function fetchTLDlist() {
+  try {
+    const res = await axios.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt', {
+      headers: { 'User-Agent': "mcstatus.net/3.0.0 the Minecraft status bot software" }
+    });
+    const arr = res.data.split('\n').filter((el) => !el.startsWith('#'));
+    TDLList = arr;
+    console.log("fetched tld list");
+  } catch (error) {
+    console.log(error.stack || error);
+    console.log("could not fetch tlds domain validation will be off");
+  }
+  return;
+}
+fetchTLDlist();
+
+
+module.exports = function ParserIP(address) {
+  let match = address.match(ipv6Pattern) || address.match(ipv6NoBracketsPattern) || address.match(ipv4Pattern) || address.match(domainPattern);
+
+  const result = {
+    domain: null,
+    ipv4: null,
+    ipv6: null,
+    port: null,
+    invalidDomain: false
+  };
+
+  if (match && match.groups) {
+    result.domain = match.groups.domain || null;
+    result.ipv4 = match.groups.ipv4 || null;
+    result.ipv6 = match.groups.ipv6 || null;
+    result.port = match.groups.port || null;
+
+    if (match.groups.port) {
+      const port = parseInt(match.groups.port, 10);
+      if (port >= 0 && port <= 65535) {
+        result.port = port;
+      }
+    }
+
+    if (result.domain) {
+      const domainParts = result.domain.split('.');
+      const tld = domainParts[domainParts.length - 1].toUpperCase();
+      if (TDLList.length >= 1 && !TDLList.includes(tld)) {
+        result.invalidDomain = true;
+      }
+    }
+  }
+  return result;
+}
